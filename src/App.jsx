4faf8f3fd3725e16a63b54ba1f7379e743446d8c
priyauday-lakshmi <priyauday130@gmail.com>
2023-12-14 20:48:24 +0800

    landingpagechanges1

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,14 @@ import Iconsgrid from './components/Iconsgrid'
 import Testtimonials from './components/Testtimonials'
 import Footer from './components/Footer'
 import img1 from "./images/testimonials-1.jpg"
+import img2 from "./images/testimonials-2.jpg"
+import img3 from "./images/testimonials-3.jpg"
+import img4 from "./images/bg-showcase-1.jpg"
+import img5 from "./images/bg-showcase-2.jpg"
+import img6 from "./images/bg-showcase-3.jpg"
+
+
+
 
 
 
@@ -14,18 +22,18 @@ function App() {
     {
       h3:"Fully Responsive Design",
       p:"When you use a theme created by Start Bootstrap, you know that the theme will look great on any device, whether it's a phone, tablet, or desktop the page will behave responsively!",
-      imageUrl:"./src/images/bg-showcase-1.jpg",
+      imageUrl:img4,
     },
     {
       h3:"Bootstrap 5 Ready",
       p:"Featuring the latest build of the new Bootstrap 5 framework!",
-      imageUrl:"./src/images/bg-showcase-2.jpg",
+      imageUrl:img5,
 
     },
     {
       h3:"Easy to Use",
       p:"Landing Page is just HTML and CSS with a splash of SCSS for users who demand some deeper customization options. Out of the box, just add your content and images, and your new landing page will be ready to go!",
-      imageUrl:"./src/images/bg-showcase-3.jpg"
+      imageUrl:img6,
     }
 
 
@@ -57,8 +65,8 @@ function App() {
             <h2 className="mb-5">What people are saying...</h2>
                 <div className="row">
                   <Testtimonials imageUrl={img1}h5="Margaret E." p="This is fantastic! Thanks so much guys!" />
-                  <Testtimonials imageUrl="./src/images/testimonials-2.jpg" h5="Fred S." p="Bootstrap is amazing. I've been using it to create lots of super nice landing pages." />
-                  <Testtimonials imageUrl="./src/images/testimonials-3.jpg" h5="Sarah W." p="Thanks so much for making these free resources available to us!" />
+                  <Testtimonials imageUrl={img2} h5="Fred S." p="Bootstrap is amazing. I've been using it to create lots of super nice landing pages." />
+                  <Testtimonials imageUrl={img3} h5="Sarah W." p="Thanks so much for making these free resources available to us!" />
                 </div>
 
             </div>
